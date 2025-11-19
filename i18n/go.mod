c05c086abf383a27a94b375fa8daf844bfc171ad
saltfishpr <526191197@qq.com>
2025-11-19 22:46:42 +0800

    add i18n

---
+++ i18n/go.mod
@@ -0,0 +1,5 @@
+module github.com/saltfishpr/pkg/i18n
+
+go 1.24.0
+
+require golang.org/x/text v0.31.0
---
+++ i18n/i18n.go
@@ -0,0 +1,109 @@
+package i18n
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"text/template"
+
+	"golang.org/x/text/language"
+)
+
+var ErrLanguageNotSupported = errors.New("language not supported")
+
+type Options struct {
+	Fallback language.Tag
+	Arg      any // argument for [Get] method
+}
+
+type Option func(*Options)
+
+func WithFallback(fallback language.Tag) Option {
+	return func(o *Options) {
+		o.Fallback = fallback
+	}
+}
+
+func WithArg(arg any) Option {
+	return func(o *Options) {
+		o.Arg = arg
+	}
+}
+
+var DefaultOptions = &Options{
+	Fallback: language.English,
+}
+
+type I18n interface {
+	Get(lang language.Tag, opts ...Option) (string, error)
+}
+
+type SimpleI18n struct {
+	data map[language.Tag]string
+}
+
+func NewSimpleI18n(data map[language.Tag]string) *SimpleI18n {
+	return &SimpleI18n{
+		data: data,
+	}
+}
+
+func (i *SimpleI18n) Get(lang language.Tag, opts ...Option) (string, error) {
+	options := *DefaultOptions // copy default options
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	if s, ok := i.data[lang]; ok {
+		return s, nil
+	}
+	if s, ok := i.data[options.Fallback]; ok {
+		return s, nil
+	}
+	return "", fmt.Errorf("language %s not supported: %w", lang, ErrLanguageNotSupported)
+}
+
+type TextTemplateI18n struct {
+	data map[language.Tag]*template.Template
+}
+
+func NewTextTemplateI18n() *TextTemplateI18n {
+	return &TextTemplateI18n{
+		data: make(map[language.Tag]*template.Template),
+	}
+}
+
+func (i *TextTemplateI18n) MustAdd(lang language.Tag, tpl string) *TextTemplateI18n {
+	i.data[lang] = template.Must(template.New("").Parse(tpl))
+	return i
+}
+
+func (i *TextTemplateI18n) Add(lang language.Tag, tpl string) error {
+	t, err := template.New("").Parse(tpl)
+	if err != nil {
+		return err
+	}
+	i.data[lang] = t
+	return nil
+}
+
+func (i *TextTemplateI18n) Get(lang language.Tag, opts ...Option) (string, error) {
+	options := *DefaultOptions // copy default options
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	tpl, ok := i.data[lang]
+	if !ok {
+		tpl, ok = i.data[options.Fallback]
+		if !ok {
+			return "", fmt.Errorf("language %s not supported: %w", lang, ErrLanguageNotSupported)
+		}
+	}
+
+	var buf bytes.Buffer
+	if err := tpl.Execute(&buf, options.Arg); err != nil {
+		return "", fmt.Errorf("execute template %s: %w", tpl.Name(), err)
+	}
+	return buf.String(), nil
+}
