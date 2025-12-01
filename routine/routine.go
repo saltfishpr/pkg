118b2166d3f3403d5774f773aa3f63939c0d43d0
saltfishpr <526191197@qq.com>
2025-12-02 00:01:16 +0800

    init

--- routine/routine.go
+++ routine/routine.go
@@ -5,28 +5,29 @@ import (
 	"time"
 )
 
-func RunSafe(fn func()) {
-	defer Recover()
+func RunSafe(fn func(), cleanup ...func(r interface{})) {
+	defer Recover(cleanup...)
 
 	fn()
 }
 
-func RunSafeCtx(ctx context.Context, fn func(ctx context.Context)) {
-	defer RecoverCtx(ctx)
+func RunSafeCtx(ctx context.Context, fn func(ctx context.Context), cleanup ...func(ctx context.Context, r interface{})) {
+	defer RecoverCtx(ctx, cleanup...)
 
 	fn(ctx)
 }
 
-func GoSafe(fn func()) {
-	go RunSafe(fn)
+func GoSafe(fn func(), cleanup ...func(r interface{})) {
+	go RunSafe(fn, cleanup...)
 }
 
-func GoSafeCtx(ctx context.Context, fn func(ctx context.Context)) {
-	go RunSafeCtx(ctx, fn)
+func GoSafeCtx(ctx context.Context, fn func(ctx context.Context), cleanup ...func(ctx context.Context, r interface{})) {
+	go RunSafeCtx(ctx, fn, cleanup...)
 }
 
 func RunWithTimeout(fn func(), timeout time.Duration) bool {
 	done := make(chan struct{})
+
 	GoSafe(func() {
 		fn()
 		close(done)
@@ -42,6 +43,7 @@ func RunWithTimeout(fn func(), timeout time.Duration) bool {
 
 func RunWithTimeoutCtx(ctx context.Context, fn func(ctx context.Context), timeout time.Duration) bool {
 	done := make(chan struct{})
+
 	GoSafeCtx(ctx, func(ctx context.Context) {
 		fn(ctx)
 		close(done)
