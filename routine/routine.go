864701270207e15f00876cb7729516e5a74dde04
saltfishpr <526191197@qq.com>
2026-01-15 22:43:35 +0800

    feat: routine remove unused ctx functions

--- routine/routine.go
+++ routine/routine.go
@@ -1,7 +1,6 @@
 package routine
 
 import (
-	"context"
 	"time"
 )
 
@@ -11,18 +10,8 @@ func RunSafe(fn func(), cleanup ...func(r interface{})) {
 	fn()
 }
 
-func RunSafeCtx(ctx context.Context, fn func(ctx context.Context), cleanup ...func(ctx context.Context, r interface{})) {
-	defer RecoverCtx(ctx, cleanup...)
-
-	fn(ctx)
-}
-
 func GoSafe(fn func(), cleanup ...func(r interface{})) {
 	go RunSafe(fn, cleanup...)
-}
-
-func GoSafeCtx(ctx context.Context, fn func(ctx context.Context), cleanup ...func(ctx context.Context, r interface{})) {
-	go RunSafeCtx(ctx, fn, cleanup...)
 }
 
 func RunWithTimeout(fn func(), timeout time.Duration) bool {
@@ -30,22 +19,6 @@ func RunWithTimeout(fn func(), timeout time.Duration) bool {
 
 	GoSafe(func() {
 		fn()
-		close(done)
-	})
-
-	select {
-	case <-done:
-		return true
-	case <-time.After(timeout):
-		return false
-	}
-}
-
-func RunWithTimeoutCtx(ctx context.Context, fn func(ctx context.Context), timeout time.Duration) bool {
-	done := make(chan struct{})
-
-	GoSafeCtx(ctx, func(ctx context.Context) {
-		fn(ctx)
 		close(done)
 	})
 
