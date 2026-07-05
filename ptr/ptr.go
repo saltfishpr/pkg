e408818361949fe1f3ca0e7078369d96d8157b9d
saltfishpr <526191197@qq.com>
2026-07-05 10:20:30 +0800

    feat(ptr): add To function

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -3,10 +3,19 @@
 // This is useful when constructing struct literals or calling APIs that accept
 // pointer fields, avoiding the need for a temporary variable:
 //
-//	req := &pb.Request{PageSize: ptr.To(int32(20))}
+//	req := &pb.Request{PageSize: ptr.Of(int32(20))}
 package ptr
 
-// To returns a pointer to a shallow copy of v.
-func To[T any](v T) *T {
+// To dereferences p, returning the zero value for T when p is nil.
+func To[T any](p *T) T {
+	if p == nil {
+		var v T
+		return v
+	}
+	return *p
+}
+
+// Of returns a pointer to a shallow copy of v.
+func Of[T any](v T) *T {
 	return &v
 }
